bbb3d4c0e0cca899f85f65fd0863126164a3399c
TatyanaGanya <121543437+TatyanaGanya@users.noreply.github.com>
2023-09-12 16:18:16 +0300

    исправлен port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/error-handler');
 const NotFoundError = require('./errors/NotFoundError');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
